b7e3a630ac2d5c01078d54a10927afb4f17e3bc5
aghiles-abib <aghiles.abib@gmail.com>
2025-10-26 12:36:59 +0100

    add multiple consumer

--- src/main/java/com/demo/rabbitmq/Consumer2.java
+++ src/main/java/com/demo/rabbitmq/Consumer2.java
@@ -1,28 +1,25 @@
 package com.demo.rabbitmq;
 
-
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class Publisher {
+public class Consumer2 {
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-       //tring message = "First message from RabbitMQ";
-        String [] messages= {"one","two","three","four"};
-        for(var m : messages){
-            channel.basicPublish("","Queue-1",null,m.getBytes());
+        DeliverCallback deliverCallback = (consumerTag,delivery)->{
+            String message = new String(delivery.getBody());
+            System.out.println("Message received = "+message);
+        };
 
-        }
+        channel.basicConsume("Queue-1",true,deliverCallback,consumerTag ->{});
 
-        channel.close();
-        connection.close();;
     }
-
 }
